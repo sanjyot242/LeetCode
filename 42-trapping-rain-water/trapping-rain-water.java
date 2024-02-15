b48c967c216ccacb326840506857f5d07290d3a7
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-02-15 14:30:08 -0800

    Update trapping-rain-water.java

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -22,3 +22,28 @@ class Solution {
         return res;
     }
 }
+
+//Brute Force
+// class Solution {
+//     public int trap(int[] height) {
+       
+//         int res = 0 ;
+        
+//         for (int i = 0 ; i < height.length;i++){
+//             int maxLeft =0,maxRight=0;
+//             int j = i ;
+//             while(j<height.length){
+//                 maxRight = Math.max(maxRight,height[j]);
+//                 j++;
+//             }
+//             j=i;
+//             while(j>=0){
+//                 maxLeft = Math.max(maxLeft,height[j]);
+//                 j--;
+//             }
+
+//             res += Math.min(maxLeft,maxRight)- height[i];
+//         }
+//         return res;
+//     }
+// }
