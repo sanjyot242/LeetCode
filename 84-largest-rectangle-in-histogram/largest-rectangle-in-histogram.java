a4b29c51c2203cc020d71f43ad9572c6470a1849
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-02-22 14:42:06 -0800

    Time: 69 ms (55.67%) | Memory: 57.1 MB (70.19%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,38 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] leftSmall = new int[heights.length];
-        int[] rightSmall = new int[heights.length];
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i = 0 ; i < heights.length;i++){
-            while(!stack.isEmpty()&& heights[stack.peek()] >= heights[i]){
-                stack.pop();
-            }
-
-            if(stack.isEmpty()) leftSmall[i]=0;
-            else leftSmall[i] = stack.peek()+1;
-            stack.push(i);
-        }
-
-        while (!stack.isEmpty()) stack.pop();
-
-        for(int i=heights.length-1;i>=0;i--){
-            while(!stack.isEmpty()&& heights[stack.peek()] >= heights[i]){
-                stack.pop();
-            }
-            if(stack.isEmpty()) rightSmall[i]=heights.length-1;
-            else rightSmall[i] = stack.peek()-1;
-            stack.push(i);
-        }
-
+        Stack<Integer> st = new Stack<Integer>();
         int maxA = 0;
-        for (int i = 0; i < heights.length ; i++) {
-            maxA = Math.max(maxA, heights[i] * (rightSmall[i] - leftSmall[i] + 1));
+        int n = heights.length;
+        for(int i = 0 ; i<= n ; i++){
+            while(!st.isEmpty() && (i==n || heights[st.peek()]>= heights[i])){
+                int height = heights[st.peek()];
+                st.pop();
+                int width ;
+                if(st.isEmpty()) width = i;
+                else width = i - st.peek() - 1;
+                maxA = Math.max(maxA , height*width);
+            }
+            st.push(i);
         }
         return maxA;
     }
 }
 
+//brute force
 // int maxArea = 0;
 //         for(int i = 0 ; i <heights.length;i++){
 //              int minHeight = Integer.MAX_VALUE;
@@ -42,3 +28,36 @@ class Solution {
 //              }
 //         }
 //         return maxArea;
+
+
+// Optimal 1 
+// int[] leftSmall = new int[heights.length];
+//         int[] rightSmall = new int[heights.length];
+//         Stack<Integer> stack = new Stack<>();
+
+//         for(int i = 0 ; i < heights.length;i++){
+//             while(!stack.isEmpty()&& heights[stack.peek()] >= heights[i]){
+//                 stack.pop();
+//             }
+
+//             if(stack.isEmpty()) leftSmall[i]=0;
+//             else leftSmall[i] = stack.peek()+1;
+//             stack.push(i);
+//         }
+
+//         while (!stack.isEmpty()) stack.pop();
+
+//         for(int i=heights.length-1;i>=0;i--){
+//             while(!stack.isEmpty()&& heights[stack.peek()] >= heights[i]){
+//                 stack.pop();
+//             }
+//             if(stack.isEmpty()) rightSmall[i]=heights.length-1;
+//             else rightSmall[i] = stack.peek()-1;
+//             stack.push(i);
+//         }
+
+//         int maxA = 0;
+//         for (int i = 0; i < heights.length ; i++) {
+//             maxA = Math.max(maxA, heights[i] * (rightSmall[i] - leftSmall[i] + 1));
+//         }
+//         return maxA;
