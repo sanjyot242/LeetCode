5ce24bae0aa974668e02d8dac97d667372eaf4c1
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-08-04 19:40:23 -0700

    Time: 0 ms (100.00%) | Memory: 41.6 MB (23.85%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,19 +14,40 @@
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        inOrder(root,ans);
-        return ans;
-    }
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> ans = new ArrayList<>();
+    //     inOrder(root,ans);
+    //     return ans;
+    // }
 
-    private void inOrder(TreeNode root,List ans){
-        if(root == null){
-            return;
-        }
+    // private void inOrder(TreeNode root,List ans){
+    //     if(root == null){
+    //         return;
+    //     }
         
-        inOrder(root.left,ans);
-        ans.add(root.val);
-        inOrder(root.right,ans);        
+    //     inOrder(root.left,ans);
+    //     ans.add(root.val);
+    //     inOrder(root.right,ans);        
+    // }
+
+     public List<Integer> inorderTraversal(TreeNode root) {
+     List <Integer> ans = new ArrayList<>();
+     Stack <TreeNode> st = new Stack<>();
+    
+    if(root == null) return ans;
+
+    while(true){
+        if(root!=null){
+            st.push(root);
+            root = root.left;
+        }else{
+            if(st.isEmpty()) break ;
+            root = st.pop();
+            ans.add(root.val);
+            root = root.right;
+        }
     }
+    
+return ans;
+     }
 }
